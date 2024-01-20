19c11af168851812603ecd93dd52da782073f8c7
Sweetlord16 <darklorderic16@gmail.com>
2024-01-20 12:25:24 +0000

    Prueba 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Planta rosa = new Planta("Rosa", "Rosa floribunda", 40);
-        Planta cactus = new Planta("Cactus", "Cactaceae", 10);
+        Planta rosa = new Planta("Rosa", "Rosa Moribunda", 40);
+        Planta margarita = new Planta("Margarita", "Margarita chica", 30);
 
-        System.out.println("¿La rosa está floreciendo? " + rosa.isFloreciendo());
-        System.out.println("¿El cactus está floreciendo? " + cactus.isFloreciendo());
+        System.out.println("¿La rosa está floreciendo?" + rosa.isFloreciendo());
+        System.out.println("¿La margarita está floreciendo?" + margarita.isFloreciendo());
 
         System.out.println(rosa.toString());
-        System.out.println(cactus.toString());
+        System.out.println(margarita.toString());
     }
 }
--- src/Planta.java
+++ src/Planta.java
@@ -1,13 +1,12 @@
 public class Planta {
     private String nombre;
-    private String nombrecientifico;
-    private  boolean floreciendo;
-
+    private String nombre_cientifico;
     private int altura;
+    private boolean floreciendo;
 
-    public Planta(String nombre, String nombrecientifico, int altura) {
+    public Planta(String nombre, String nombre_cientifico, int altura) {
         this.nombre = nombre;
-        this.nombrecientifico = nombrecientifico;
+        this.nombre_cientifico = nombre_cientifico;
         this.altura = altura;
     }
 
@@ -15,26 +14,26 @@ public class Planta {
         return nombre;
     }
 
-    public String getNombrecientifico() {
-        return nombrecientifico;
-    }
-
-    public boolean isFloreciendo() {
-        floreciendo = altura>30;
-        return floreciendo;
+    public String getNombre_cientifico() {
+        return nombre_cientifico;
     }
 
     public int getAltura() {
         return altura;
     }
 
+    public boolean isFloreciendo() {
+        floreciendo = altura >30;
+        return floreciendo;
+    }
+
     @Override
     public String toString() {
         return "Planta{" +
                 "nombre='" + nombre + '\'' +
-                ", nombrecientifico='" + nombrecientifico + '\'' +
-                ", floreciendo=" + floreciendo +
+                ", nombre_cientifico='" + nombre_cientifico + '\'' +
                 ", altura=" + altura +
+                ", floreciendo=" + floreciendo +
                 '}';
     }
 }
